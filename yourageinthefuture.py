1dab5d78f0232032b3fabb0bed03c7781bb0d22e
Miruton <jesusjimenez141993@gmail.com>
2022-07-21 16:53:28 -0400

    Update yourageinthefuture.py

--- yourageinthefuture.py
+++ yourageinthefuture.py
@@ -1,12 +1,18 @@
 x = input('What is your name?: ')
-
 y = int(input('Can you enter your current age?: '))
 
 
-if y>0:
-    m=y+100
-    print("Hello",x ,"Nice to meet you")
-    print("In the year 2122 you are turning",m,'Years Old')
+while y<1:
+    
+    print("Hello",x,"Nice to meet you")
+    print("Please enter an age higher than 0")    
+    
+
+else:
+    print("Your age is",y)
+
+    z=2022+y    
+    print('Did you know by the year',z,'you are turning 100 Years Old')
 
         
 

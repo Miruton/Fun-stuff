4cdf96938ee43d314e2c314e45c88146a3bde9f5
Miruton <jesusjimenez141993@gmail.com>
2022-07-21 16:28:31 -0400

    Update yourageinthefuture.py

--- yourageinthefuture.py
+++ yourageinthefuture.py
@@ -1,9 +1,3 @@
-"""Create a program that asks the user to enter their name and their age.
-Print out a message addressed to them that tells them the year that they
-will turn 100 years old. Note: for this exercise, the expectation is that
-you explicitly write out the year (and therefore be out of date the next year)."""
-
-
 x = input('What is your name?: ')
 
 y = int(input('Can you enter your current age?: '))

f203de13951e60c11bdd473ead918d25a2ddedc7
Miruton <jesusjimenez141993@gmail.com>
2022-07-21 17:41:14 -0400

    Update yourageinthefuture.py
    Updated - Now shows how old you are when turn 100 years old.

--- yourageinthefuture.py
+++ yourageinthefuture.py
@@ -1,18 +1,23 @@
 x = input('What is your name?: ')
-y = int(input('Can you enter your current age?: '))
+
+print("Hello",x,"Nice to meet you")
+
+y = int(input('Can you enter your current age?:'))
 
 
 while y<1:
-    
-    print("Hello",x,"Nice to meet you")
-    print("Please enter an age higher than 0")    
+      
+    y = int(input('Please enter an age higher than 0: '))
     
 
 else:
-    print("Your age is",y)
 
-    z=2022+y    
-    print('Did you know by the year',z,'you are turning 100 Years Old')
+    print("Your age is",y)
+    c=100-y
+    a=2022+c
+
+
+    print('Did you know by the year',a,'you are turning 100 Years Old')
 
         
 

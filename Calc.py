3722fe2ec026836049bd1083d0eccadea53a23e3
Miruton <jesusjimenez141993@gmail.com>
2022-07-25 21:41:53 -0400

    Create Calc.py
    Basic Calculator

---
+++ Calc.py
@@ -0,0 +1,52 @@
+x = 0
+y = 0
+d = 0
+
+
+print("Welcome to Operator Calculator")
+c = int(input("""PLease select the number of the operator you wish to work with:
+1->Addition
+2->Substraction
+3->Multiplication
+4->Division:
+Enter the number with the Keyboard:"""))
+
+if c==1:
+        print("You are adding values")
+        x= int(input('Enter First value:'))
+        y= int(input('Enter Second Value:'))
+        d=x+y
+        print("Result is: ",d)
+ 
+elif c==2:
+        print("You are substracting values")
+        x=int(input('Enter First value:'))
+        y=int(input('Enter Second Value:'))
+        d=x-y
+        print("Result is: ",d)
+elif c==3:
+        print("You are Multiplying values")
+        x=int(input('Enter First value:'))
+        y=int(input('Enter Second Value:'))
+        d=x*y
+        print("Result is: ",d)
+
+elif c==4:
+        print("You are Dividing values")
+        x=int(input('Enter First value:'))
+        y=int(input('Enter Second Value:'))
+        d=x/y
+        print("Result is: ",d)
+else:
+        print("Please select one of the operators: ",d)
+
+
+
+
+        
+
+    
+
+
+
+    
